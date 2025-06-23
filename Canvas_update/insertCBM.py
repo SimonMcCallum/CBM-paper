4f9f3f3bc2247a2c5f02bc3b3f6eb027cb3e0df3
Simon McCallum <simon.mccallum@vuw.ac.nz>
2025-06-23 16:48:28 +0100

    Adding Documentation and question start

---
+++ Canvas_update/insertCBM.py
@@ -0,0 +1,3 @@
+# Python code that inserts a Confidence Based Marking System into a set of questions in QTI format.
+# Takes a zip file - raw canvas output - and creates a new zip with the updated questions.
+
---
+++ Canvas_update/todo.md
@@ -0,0 +1,4 @@
+# Task for creating the python code
+Key [ ] means open task, [~] means work in progress, [?] means needs review, [x] means done
+
+## Tasks
